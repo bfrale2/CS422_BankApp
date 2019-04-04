c5af76a867b5e9b51788ba1c3f7faf08711986cf
Brandon Frale <bfrale2@uic.edu>
2019-04-04 15:37:04 -0500

    Working commit of login activity

--- app/src/main/java/edu/uic/group19/a422ndbank/LoginActivity.java
+++ app/src/main/java/edu/uic/group19/a422ndbank/LoginActivity.java
@@ -1,13 +1,31 @@
 package edu.uic.group19.a422ndbank;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements CredentialsFragment.OnFragmentInteractionListener, LoginProgressFragment.OnFragmentInteractionListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment credentialsFragment = new CredentialsFragment();
+        fm.beginTransaction().add(R.id.frame_loginStep, credentialsFragment).commit();
+
+        Fragment loginProgressFragment = new LoginProgressFragment();
+        fm.beginTransaction().add(R.id.frame_loginProgress, loginProgressFragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
