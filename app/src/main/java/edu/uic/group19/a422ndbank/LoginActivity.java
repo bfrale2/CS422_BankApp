87e9d7b5edbbf1c99eee60808eeedff85b31054c
Brandon Frale <bfrale2@uic.edu>
2019-04-04 13:25:17 -0500

    first commit

---
+++ app/src/main/java/edu/uic/group19/a422ndbank/LoginActivity.java
@@ -0,0 +1,13 @@
+package edu.uic.group19.a422ndbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
