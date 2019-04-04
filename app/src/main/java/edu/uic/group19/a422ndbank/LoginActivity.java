c04952264d897afdafbc17a4b7592d5b708a1b02
Brandon Frale <bfrale2@uic.edu>
2019-04-04 17:10:19 -0500

    Finished LoginActivity

--- app/src/main/java/edu/uic/group19/a422ndbank/LoginActivity.java
+++ app/src/main/java/edu/uic/group19/a422ndbank/LoginActivity.java
@@ -8,26 +8,65 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity implements CredentialsFragment.OnFragmentInteractionListener, LoginProgressFragment.OnFragmentInteractionListener {
+import edu.uic.group19.a422ndbank.MainApp.MainActivity;
+
+public class LoginActivity extends AppCompatActivity implements CredentialsFragment.OnFragmentInteractionListener, TwoFactorFragment.OnFragmentInteractionListener {
+
+    LoginProgressFragment loginProgressFragment;
+    CredentialsFragment credentialsFragment;
+    TwoFactorFragment twoFactorFragment;
 
 
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        twoFactorFragment = new TwoFactorFragment();
+
         FragmentManager fm = getSupportFragmentManager();
 
-        Fragment credentialsFragment = new CredentialsFragment();
+        credentialsFragment = new CredentialsFragment();
         fm.beginTransaction().add(R.id.frame_loginStep, credentialsFragment).commit();
 
-        Fragment loginProgressFragment = new LoginProgressFragment();
+        loginProgressFragment = new LoginProgressFragment();
         fm.beginTransaction().add(R.id.frame_loginProgress, loginProgressFragment).commit();
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
 
+    //
+    @Override
+    public void onCredentialsEntered(String username, String password) {
+        if(!username.isEmpty() && !password.isEmpty()) {
+
+            loginProgressFragment.setProgressCheck(LoginProgressFragment.STEP_CREDNETIALS, true);
+
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.frame_loginStep, twoFactorFragment).commit();
+        }
+    }
+
+
+    //
+    @Override
+    public void onTwoFactorChanged(String code) {
+        if(code.length() == 6) {
+            loginProgressFragment.setProgressCheck(LoginProgressFragment.STEP_TWO_FACTOR, true);
+            loginProgressFragment.setLockStatus(false);
+            twoFactorFragment.enableSubmit(true);
+        } else {
+            loginProgressFragment.setProgressCheck(LoginProgressFragment.STEP_TWO_FACTOR, false);
+            loginProgressFragment.setLockStatus(true);
+            twoFactorFragment.enableSubmit(false);
+        }
+    }
+
+    @Override
+    public void onTwoFactorSubmit(String code) {
+        if(code.length() == 6) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/edu/uic/group19/a422ndbank/TwoFactorFragment.java
+++ app/src/main/java/edu/uic/group19/a422ndbank/TwoFactorFragment.java
@@ -1,12 +1,15 @@
 package edu.uic.group19.a422ndbank;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -18,62 +21,73 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class TwoFactorFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
+    private EditText fieldSecurityCode;
+    private Button btnSubmit;
+
+
+    //
     public TwoFactorFragment() {
         // Required empty public constructor
     }
+
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment TwoFactorFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static TwoFactorFragment newInstance(String param1, String param2) {
+    public static TwoFactorFragment newInstance() {
         TwoFactorFragment fragment = new TwoFactorFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
+
+    //
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
+
+    //
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two_factor, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_two_factor, container, false);
+
+        fieldSecurityCode = rootView.findViewById(R.id.edit_securityCode);
+        fieldSecurityCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mListener.onTwoFactorChanged(fieldSecurityCode.getText().toString());
+            }
+        });
+
+        btnSubmit = rootView.findViewById(R.id.btn_enterCode);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onTwoFactorSubmit(fieldSecurityCode.getText().toString());
+            }
+        });
+
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
+    //
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -85,11 +99,14 @@ public class TwoFactorFragment extends Fragment {
         }
     }
 
+
+    //
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
+
 
     /**
      * This interface must be implemented by activities that contain this
@@ -102,7 +119,13 @@ public class TwoFactorFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onTwoFactorChanged(String code);
+        void onTwoFactorSubmit(String code);
+    }
+
+
+    //
+    public void enableSubmit(boolean enabled) {
+        btnSubmit.setEnabled(true);
     }
 }
