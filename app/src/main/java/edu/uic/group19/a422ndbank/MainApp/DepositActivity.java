53edbd3670d04c79f7c8ea59a6849346b519b82a
Himanshu Dhamija <Himanshu@10-55-218-129.vlan860.client.wireless.uic.edu>
2019-04-04 21:39:49 -0500

    depositfile

--- app/src/main/java/edu/uic/group19/a422ndbank/MainApp/DepositActivity.java
+++ app/src/main/java/edu/uic/group19/a422ndbank/MainApp/DepositActivity.java
@@ -1,15 +1,60 @@
 package edu.uic.group19.a422ndbank.MainApp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.uic.group19.a422ndbank.R;
 
-public class DepositActivity extends AppCompatActivity {
+public class DepositActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button check_deposit;
+    private Button cash_deposit;
+    private Button home_button;
+    private Button help_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deposit);
+        initializeAllViews();
+
+        check_deposit.setOnClickListener(this);
+        cash_deposit.setOnClickListener(this);
+        help_button.setOnClickListener(this);
+        home_button.setOnClickListener(this);
+    }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.check_deposit:
+                    startActivity(new Intent(this, Depositactivity_upload.class));
+                    break;
+
+                case R.id.cash_deposit:
+                    openDailog();
+                    break;
+
+                case R.id.trans_hist_homeButton:
+                    finish();
+                    break;
+            }
+        }
+
+    public void openDailog()
+    {
+        dailogclass exampleDialog = new dailogclass();
+        exampleDialog.show(getSupportFragmentManager(), "Information");
+    }
+
+    private void initializeAllViews() {
+        check_deposit = findViewById(R.id.check_deposit);
+        cash_deposit = findViewById(R.id.cash_deposit);
+        home_button = findViewById(R.id.trans_hist_homeButton);
+        help_button= findViewById(R.id.help_deposit);
+
     }
 }
---
+++ app/src/main/java/edu/uic/group19/a422ndbank/MainApp/Depositactivity_upload.java
@@ -0,0 +1,14 @@
+package edu.uic.group19.a422ndbank.MainApp;
+
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.uic.group19.a422ndbank.R;
+
+
+public class Depositactivity_upload extends AppCompatActivity {
+}
