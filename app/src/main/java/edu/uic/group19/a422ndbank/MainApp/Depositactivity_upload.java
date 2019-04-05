78072e27e66ae45dc432b40f79fcf94adfc41165
Himanshu Dhamija <Himanshu@10-55-218-129.vlan860.client.wireless.uic.edu>
2019-04-04 23:29:52 -0500

    depoist3

--- app/src/main/java/edu/uic/group19/a422ndbank/MainApp/Depositactivity_upload.java
+++ app/src/main/java/edu/uic/group19/a422ndbank/MainApp/Depositactivity_upload.java
@@ -1,14 +1,34 @@
 package edu.uic.group19.a422ndbank.MainApp;
 
-import android.app.AppComponentFactory;
-import android.content.Intent;
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+//import android.widget.Button;
 
 import edu.uic.group19.a422ndbank.R;
 
+public class Depositactivity_upload extends AppCompatActivity implements View.OnClickListener{
 
-public class Depositactivity_upload extends AppCompatActivity  {
+//    private Button camera;
+//    private Button home_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.depositupload);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+//    private void initializeAllViews() {
+//
+//        camera = findViewById(R.id.camera);
+//        home_button = findViewById(R.id.trans_hist_homeButton);
+//
+//
+//    }
 }
